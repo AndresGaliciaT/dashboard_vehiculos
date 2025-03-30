27d567abfb0b78abef6da8296f4baec8f6a23bc9
Andres Galicia <a18020005@itsperote.edu.mx>
2025-03-30 16:48:36 -0600

    Descripción de los cambios realizados

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-# Cargar el dataset (si ya lo tienes en tu repositorio)
-car_data = pd.read_csv('vehicles_us.csv')
-
-# Título de la app
-st.header('Panel de Control de Ventas de Coches')
-
-# Botón para construir el histograma
-hist_button = st.button('Construir histograma')
-
-if hist_button:
-    st.write('Creando un histograma...')
-    fig = px.histogram(car_data, x="odometer")  # Cambia el nombre de la columna según tu dataset
-    st.plotly_chart(fig)
+import streamlit as st
+        
+car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+hist_button = st.button('Construir histograma') # crear un botón
+        
+if hist_button: # al hacer clic en el botón
+            # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+            
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+     # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
